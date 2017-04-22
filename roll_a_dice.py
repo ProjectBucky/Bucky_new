7852e656395ababf8b45273079ce385e2b00ad62
Gautam krishna.R <r.gautamkrishna@gmail.com>
2017-04-22 21:14:42 +0530

    expanded for n dice

--- roll_a_dice.py
+++ roll_a_dice.py
@@ -4,3 +4,17 @@ def roll_a_dice():
 	choice = random.choice(face)
 	numstr = ["one","two","three","four","five","six","seven","eight"]
 	return "Dice rolled and Its "+str(numstr[face.index(choice)])
+def roll_n_dice(num):
+	face=range(1,7)
+	choice = list()
+	numstr = ["one","two","three","four","five","six","seven","eight"]
+	for i in range(0,num):
+		choice.append(random.choice(face))
+	retstr = "Rolled "+str(num)+" dice, got "
+	for i in range(0,len(choice)):
+		retstr += str(numstr[face.index(choice[i])])
+		if i==(len(choice)-2):
+			retstr += " and "
+		elif i!=(len(choice)-1):
+			retstr += ", "
+	return retstr
